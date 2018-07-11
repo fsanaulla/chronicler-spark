504e9d03ac2ede2d0341b6fb33f37faec002dcf7
fsanaulla <fayaz.sanaulla@gmail.com>
2018-07-11 15:39:34 +0300

    documentation for streaming api

--- spark-streaming/src/main/scala/com/github/fsanaulla/chronicler/spark/streaming/package.scala
+++ spark-streaming/src/main/scala/com/github/fsanaulla/chronicler/spark/streaming/package.scala
@@ -6,13 +6,27 @@ import org.apache.spark.streaming.dstream.DStream
 
 package object streaming {
 
-  implicit final class DStreamOps[T](private val dstream: DStream[T]) extends AnyVal {
-    def savaToInflux(dbName: String,
+  /**
+    * Extension that will provide static methods for saving DStream to InfluxDB
+    *
+    * @param stream - Spark DStream
+    * @tparam T  - DStream inner type
+    */
+  implicit final class DStreamOps[T](private val stream: DStream[T]) extends AnyVal {
+
+    /**
+      * Write stream to influxdb
+      *
+      * @param dbName   - InfluxDB name
+      * @param measName - measurement name
+      * @param wr       - implicit influx writer
+      */
+    def saveToInflux(dbName: String,
                      measName: String)(implicit wr: InfluxWriter[T], conf: InfluxConfig): Unit = {
 
       val influx = Influx.io(conf)
       val meas = influx.measurement[T](dbName, measName)
-      dstream.foreachRDD { rdd =>
+      stream.foreachRDD { rdd =>
         rdd.foreachPartition { part =>
           part.foreach { t =>
             meas.write(t)
