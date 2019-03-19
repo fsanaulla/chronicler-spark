fcccd8fc87690c623d55540790a70a79ffa303ae
fsanaulla <fayaz.sanaulla@gmail.com>
2019-03-19 17:20:39 +0200

    [ALL][RELEASE-0.2.7] - bump chronicler

--- project/Library.scala
+++ project/Library.scala
@@ -3,7 +3,7 @@ import sbt._
 object Library {
   
   object Versions {
-    val chronicler = "0.4.5"
+    val chronicler = "0.4.6"
     val spark      = "2.4.0"
     val scalaTest  = "3.0.5"
     val scalaCheck = "1.14.0"
